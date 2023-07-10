32860a70cfc5eec451f79c7f613baa73e296759c
gokul-chandrasekar <113445140+gokul-chandrasekar@users.noreply.github.com>
2023-07-10 22:10:54 +0530

    Create 4.Simple calculator.R

---
+++ 4.Simple calculator.R
@@ -0,0 +1,30 @@
+add <- function(a, b) {
+  return(a + b)
+}
+
+# Function for subtraction
+subtract <- function(a, b) {
+  return(a - b)
+}
+
+# Function for multiplication
+multiply <- function(a, b) {
+  return(a * b)
+}
+
+# Function for division
+divide <- function(a, b) {
+  return(a / b)
+}
+
+# Example usage
+result_add <- add(5, 3)
+result_subtract <- subtract(10, 4)
+result_multiply <- multiply(2, 6)
+result_divide <- divide(10, 2)
+
+# Printing the results
+print(result_add)
+print(result_subtract)
+print(result_multiply)
+print(result_divide)
